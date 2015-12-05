9ad4256f4e7ce3d29a78dcdbfce376d3126678be
BlackDizel <blackdizel@gmail.com>
2015-12-06 00:22:56 +0300

    add data provider stub class

---
+++ app/src/main/java/ru/byters/bcmapnko/utils/LocalData.java
@@ -0,0 +1,17 @@
+package ru.byters.bcmapnko.utils;
+
+import java.util.List;
+
+import ru.byters.bcmapnko.model.Task;
+
+public class LocalData {
+    public static void saveData(List<Task> items) {
+    }
+
+    public static void addData(Task item) {
+    }
+
+    public static List<Task> readData() {
+        return null;
+    }
+}
