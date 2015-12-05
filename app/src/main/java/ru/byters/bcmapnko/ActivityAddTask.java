41389bb0e15bed7f1fd8cc3bf58d0db74fc41182
BlackDizel <blackdizel@gmail.com>
2015-12-05 23:12:00 +0300

    add activity select position stub

--- app/src/main/java/ru/byters/bcmapnko/ActivityAddTask.java
+++ app/src/main/java/ru/byters/bcmapnko/ActivityAddTask.java
@@ -1,13 +1,47 @@
 package ru.byters.bcmapnko;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class ActivityAddTask extends AppCompatActivity {
+public class ActivityAddTask extends AppCompatActivity implements View.OnClickListener {
+
+    public static int REQUEST_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        findViewById(R.id.bSelectLocation).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_task, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add_task) {
+            //todo save task and return
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivityForResult(new Intent(this, ActivitySelectLocation.class), REQUEST_LOCATION);
     }
 }
