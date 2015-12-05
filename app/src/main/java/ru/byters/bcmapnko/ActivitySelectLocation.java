49e5654776203a1b69abba15d75cf6967b74f301
BlackDizel <blackdizel@gmail.com>
2015-12-06 00:10:16 +0300

    add select location data

--- app/src/main/java/ru/byters/bcmapnko/ActivitySelectLocation.java
+++ app/src/main/java/ru/byters/bcmapnko/ActivitySelectLocation.java
@@ -1,46 +1,85 @@
 package ru.byters.bcmapnko;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ActivitySelectLocation extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
+public class ActivitySelectLocation extends FragmentActivity
+        implements OnMapReadyCallback, View.OnClickListener, GoogleMap.OnMapClickListener {
+
+    public static final String INTENT_COORDS = "coords";
+    public static final String INTENT_TITLE = "title";
+    LatLng coordinates;
+    String shortLocalityName;
+    Button pickButton;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        pickButton = (Button) findViewById(R.id.queryButton);
+        pickButton.setOnClickListener(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapClickListener(this);
+/*
+        LatLng pos = new LatLng(55.7522, 37.6155);
+        mMap.addMarker(new MarkerOptions().position(pos).title("Marker in Moscow"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.putExtra(INTENT_COORDS, coordinates);
+        intent.putExtra(INTENT_TITLE, shortLocalityName);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mMap.clear();
+        pickButton.setVisibility(View.GONE);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(""));
+
+        Geocoder geocoder = new Geocoder(ActivitySelectLocation.this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            coordinates = latLng;
+
+            String address = "";
+            for (int i = 0; i < addresses.get(0).getMaxAddressLineIndex(); ++i)
+                address += addresses.get(0).getAddressLine(i) + " ";
+
+            shortLocalityName = address;
+            pickButton.setVisibility(View.VISIBLE);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
